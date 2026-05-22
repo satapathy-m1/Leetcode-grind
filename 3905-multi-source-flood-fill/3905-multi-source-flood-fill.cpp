7abe94292b80f92c675ec430da9bf8aa2ddda071
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-05-22 14:10:46 +0530

    Time: 217 ms (58%), Space: 238.3 MB (54.06%) - LeetHub

--- 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+    typedef tuple<int, int, int, int> tup;
     vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
         vector<vector<int>> grid(n, vector<int>(m, 0));
         vector<vector<int>> timeArr(n, vector<int>(m, INT_MAX));
-        queue<pair<int, pair<int, pair<int, int>>>> q;
+        queue<tup> q;
         
         for(auto source : sources) {
             int r = source[0], c = source[1], color = source[2];
-            q.push({color,{0, {r, c}}});
+            q.push({color, 0, r, c});
             grid[r][c] = color;
             timeArr[r][c] = 0;
         }
@@ -16,19 +17,15 @@ public:
         while(!q.empty()) {
             int sz = q.size();
             while(sz--) {
-                auto [color, secondPart] = q.front();
+                auto[color,time,r,c] = q.front();
                 q.pop();
-                int time = secondPart.first;
-                auto coordinates = secondPart.second;
-                int r = coordinates.first;
-                int c = coordinates.second;
                 for(auto [dr, dc] : directions) {
                     int nr = r + dr, nc = c + dc;
                     if(nr >= 0 && nr < n && nc >= 0 && nc < m) {
                         if(timeArr[nr][nc] > time + 1) {
                             grid[nr][nc] = grid[r][c];
                             timeArr[nr][nc] = time + 1;
-                            q.push({color, {timeArr[nr][nc], {nr, nc}}});
+                            q.push({color, time+1, nr, nc});
                         }
                         else if(timeArr[nr][nc] == time + 1) {
                             grid[nr][nc] = max(grid[nr][nc], grid[r][c]);
