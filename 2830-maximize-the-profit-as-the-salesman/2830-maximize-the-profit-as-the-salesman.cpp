39f1730642b13155353969d81d1c2c1a1fac30e7
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-05-23 12:52:37 +0530

    Time: 72 ms (54.78%), Space: 98.9 MB (91.54%) - LeetHub

--- 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.cpp
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.cpp
@@ -25,7 +25,16 @@ public:
     }
     int maximizeTheProfit(int n, vector<vector<int>>& offers) {
         sort(begin(offers), end(offers));
-        vector<int> dp(offers.size() + 1, -1);
-        return f(0, n, offers, dp);
+        int m = offers.size();
+        vector<int> dp(offers.size() + 2, 0);
+        dp[m] = INT_MIN;
+        for(int i = m - 1; i >= 0; i--) {
+            int notT = dp[i + 1];
+            int take = offers[i][2];
+            int nextIdx = bsHelper(offers[i][1], offers);
+            if(nextIdx != -1) take += dp[nextIdx];
+            dp[i] = max(notT, take);
+        }
+        return dp[0];
     }
 };
