4391a5cf05891c927382fc2bead6a89cb67a0fb6
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-05-31 11:22:31 +0530

    Time: 0 ms (100%), Space: 10.4 MB (61.52%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        int i = 0, j = n - 1;
-        bool turn = true;
-        while(i < j) {
-            if(turn) {
-                if(piles[i] >= piles[j]) i++;
-                else j--;
-            }
-            else {
-                if(piles[i] >= piles[j]) j--;
-                else i++;
-            }
-            turn = !turn;
-        }
         return true;
     }
 };
