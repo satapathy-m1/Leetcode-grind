592b0703d4ce05e764faf15ac26d438dada4edb8
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-06-03 22:53:05 +0530

    Time: 3 ms (80.35%), Space: 14.3 MB (93.83%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int f(int i, int flag, int k, int n, vector<int>& prices) {
-        if(i == n || k == 0) return 0;
-        if(dp[i][flag][k] != -1) return dp[i][flag][k];
-        int profit = 0;
-        if(flag) {
-            int buyProfit = max(-prices[i] + f(i + 1, !flag, k, n, prices), 0 + f(i + 1, flag, k, n, prices));
-            profit = max(profit, buyProfit);
-        }
-        else {
-            int sellProfit = max(prices[i] + f(i + 1, !flag, k - 1, n, prices), 0 + f(i + 1, flag, k, n, prices)); 
-            profit = max(profit, sellProfit);
-        }
-        return dp[i][flag][k] = profit;
-    }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        dp.resize(n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
         vector<vector<int>> ahead(2, vector<int>(k + 1, 0));
         for(int i = n - 1; i >= 0; i--) {
             for(int flag = 0; flag < 2; flag++) {
