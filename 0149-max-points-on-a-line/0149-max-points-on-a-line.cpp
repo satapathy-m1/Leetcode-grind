b900cb806be70f3f24c0e892bfffc07a75e08949
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-06-06 14:29:07 +0530

    Time: 43 ms (23.2%), Space: 24.2 MB (12.07%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -3,18 +3,20 @@ public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
         if(n == 1) return 1;
-        int maxi = 2;
+        int maxi = 1;
         for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                int cnt = 2;
-                double slope = (double)(points[j][1] - points[i][1]) / (points[j][0] - points[i][0]);
-                for(int k = 0; k < n; k++) {
-                    if(k == i || k == j) continue;
-                    double slope_2 = (double)(points[k][1] - points[i][1]) / (points[k][0] - points[i][0]);
-                    cnt += (slope_2 == slope);
-                    maxi = max(cnt, maxi);
+            unordered_map<double, int> mpp;
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                int dy = points[j][1] - points[i][1];
+                int dx = points[j][0] - points[i][0];
+                double angle = atan2(dy, dx);
+                if(mpp.find(angle) == mpp.end()) {
+                    mpp[angle] = 2;
                 }
+                else mpp[angle]++;
             }
+            for(auto it : mpp) maxi = max(maxi, it.second);
         }
         return maxi;
     }
