2c80728aff1a41cd47ba9f9eef4219e9c022a6dd
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-07-04 10:10:32 +0530

    Time: 99 ms (35.92%), Space: 155 MB (6.99%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -7,21 +7,22 @@ public:
             adj[u].push_back({v, d});
             adj[v].push_back({u, d});
         }
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-        pq.push({1e18, 1}); //sofar dist, node
-        vector<long long> dist(n + 1, 1e18);
-        while(!pq.empty()) {
-            auto [d, u] = pq.top();
-            pq.pop();
-            if(d != dist[u]) continue;
+        long long minWeight =  1e18;
+        queue<int> q;
+        vector<bool> vis(n + 1, 0);
+        q.push(1);
+        vis[1] = true;
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
             for(auto [v, d_v] : adj[u]) {
-                long long new_d = min(d, d_v);
-                if(new_d < dist[v]) {
-                    pq.push({new_d, v});
-                    dist[v] = new_d;
+                minWeight = min(minWeight, d_v);
+                if(!vis[v]) {
+                    q.push(v);
+                    vis[v] = true;
                 }
             }
         }
-        return dist[n];
+        return minWeight;
     }
 };
