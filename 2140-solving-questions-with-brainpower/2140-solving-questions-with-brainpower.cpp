3bc900d0bd045397ba406654294725b0d53b432c
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-07-04 19:03:42 +0530

    Time: 8 ms (51.61%), Space: 126 MB (46.86%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    vector<long long> dp;
+    long long f(int i, vector<vector<int>>& questions, int n) {
+        if(i >= n) return 0;
+        if(dp[i] != -1) return dp[i];
+        
+        long long skip = f(i + 1, questions, n);
+        long long solve = questions[i][0] + f(i + questions[i][1] + 1, questions, n);
+
+        return dp[i] = max(skip, solve);
+    }
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        dp.resize(n, -1);
+        return f(0, questions, n);
+    }
+};
