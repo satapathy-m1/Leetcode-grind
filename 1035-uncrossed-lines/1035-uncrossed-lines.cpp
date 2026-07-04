1a77db99bc3f20df9e875d8be31fe409af0539e6
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-07-04 18:24:23 +0530

    Time: 8 ms (44.68%), Space: 16.6 MB (58.59%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -16,7 +16,17 @@ private:
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size();
-        dp.resize(m, vector<int>(n, -1));
-        return f(0, 0, m, n, nums1, nums2);
+        dp.resize(m + 1, vector<int>(n + 1, 0));
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                int ways1 = 0, ways2 = 0;
+                if(nums1[i] == nums2[j]) ways1 = 1 + dp[i + 1][j + 1];
+                else {
+                    ways2 = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+                dp[i][j] = max(ways1, ways2);
+            }
+        }
+        return dp[0][0];
     }
 };
