b1ac84a9d71a35083da85027a7f8bbf1ff1c5749
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-07-13 19:49:40 +0530

    Time: 34 ms (49.18%), Space: 19.2 MB (48.82%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -11,14 +11,14 @@ private:
 public:
     int findTheLongestSubstring(string s) {
         int n = s.length();
-        unordered_map<string, int> mpp;
-        string temp = string(5, '0');
+        unordered_map<int, int> mpp;
+        int temp = 0;
         mpp[temp] = -1;
         int maxi = 0;
         for(int i = 0; i < n; i++) {
             int idx = getIdx(s[i]);
             if(idx != -1) {
-                temp[idx] = ((temp[idx] - '0' + 1) % 2) + '0';
+                temp = temp ^ (1 << (5 - 1 - idx));
             }
             if(mpp.count(temp)) maxi = max(maxi, i - mpp[temp]);
             else mpp[temp] = i;
