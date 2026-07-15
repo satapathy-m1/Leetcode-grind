f788c2a8ee54c17016cff87cfc0b6d2e13753aa2
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-07-15 11:04:05 +0530

    Time: 2 ms (18.99%), Space: 8.7 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,19 +1,6 @@
 class Solution {
-private:
-    int findSum(int n, bool isEven) {
-        int start = 1 + isEven;
-        int sum = start;
-        n = n - 1;
-        while(n--) {
-            start = start + 2;
-            sum += start;
-        }
-        return sum;
-    }
 public:
     int gcdOfOddEvenSums(int n) {
-        int sum1 = findSum(n, false);
-        int sum2 = findSum(n, true);
-        return gcd(sum1, sum2);
+        return n;
     }
 };
