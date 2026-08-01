2768d062d453d75fe33a6214c9de05bae942832d
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-08-01 12:29:19 +0530

    Time: 0 ms (100%), Space: 10.5 MB (10.22%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,16 +1,19 @@
 class Solution {
 private:
+vector<vector<int>> dp;
     int f(int i, int j, vector<int>& nums) {
         if(i > j) return 0;
-
+        if(dp[i][j] != -1) return dp[i][j];
         int c1 = nums[i] + min(f(i + 2, j, nums), f(i + 1, j - 1, nums));
         int c2 = nums[j] + min(f(i, j - 2, nums), f(i + 1, j - 1, nums));
 
-        return max(c1, c2);
+        return dp[i][j] = max(c1, c2);
     }
 public:
     bool predictTheWinner(vector<int>& nums) {
-        int i = 0, j = nums.size() - 1;
+        int n = size(nums);
+        int i = 0, j = n - 1;
+        dp.resize(n + 2, vector<int>(n + 2, -1));
         int sum = accumulate(begin(nums), end(nums), 0);
         int ans = f(i, j, nums);
         return ans >= (sum+1) / 2;
