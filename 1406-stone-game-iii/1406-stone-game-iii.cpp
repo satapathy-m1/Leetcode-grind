1d1e3606b5c8ab199bbb2074a7cb2bc48fa792e7
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-08-03 14:12:39 +0530

    Time: 52 ms (26.71%), Space: 141.7 MB (43.55%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -12,19 +12,8 @@ public:
     }
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        dp.assign(n + 1, 0);
-        int a1 = 0, a2 = 0, a3 = 0;
-        for(int i = n - 1; i >= 0; i--) {
-            int result = stoneValue[i] - a1;
-            if(i + 1 < n) result = max(result, stoneValue[i] + stoneValue[i + 1] - a2);
-            if(i + 2 < n) result = max(result, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - a3);
-
-            dp[i] = result;
-            a3 = a2;
-            a2 = a1;
-            a1 = dp[i];
-        }
-        int diff = dp[0];
+        dp.assign(n, -1);
+        int diff = f(0, stoneValue, n);
         if(diff > 0) return "Alice";
         else if(diff < 0) return "Bob";
         else return "Tie";
