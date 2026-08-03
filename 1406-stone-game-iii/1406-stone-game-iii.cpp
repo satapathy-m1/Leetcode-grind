46c129398b58175b6b768443e1c7419392fe6ce5
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-08-03 14:13:34 +0530

    Time: 57 ms (24.18%), Space: 142.5 MB (28.49%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,21 +1,26 @@
 class Solution {
-public:
+private:
+    int n;
     vector<int> dp;
-    int f(int i, vector<int>& stoneValue, int n) {
+    int f(int i, vector<int>& stoneValue) {
         if(i >= n) return 0;
         if(dp[i] != -1) return dp[i];
-        int result = stoneValue[i] - f(i + 1, stoneValue, n);
-        if(i + 1 < n) result = max(result, stoneValue[i] + stoneValue[i + 1] - f(i + 2, stoneValue, n));
-        if(i + 2 < n) result = max(result, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - f(i + 3, stoneValue, n));
+
+        int result = stoneValue[i] - f(i + 1, stoneValue);
+        if(i + 1 < n) result = max(result, stoneValue[i] + stoneValue[i + 1] - f(i + 2, stoneValue));
+        if(i + 2 < n) {
+            result = max(result, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - f(i + 3, stoneValue));
+        }
 
         return dp[i] = result;
     }
+public:
     string stoneGameIII(vector<int>& stoneValue) {
-        int n = stoneValue.size();
-        dp.assign(n, -1);
-        int diff = f(0, stoneValue, n);
-        if(diff > 0) return "Alice";
-        else if(diff < 0) return "Bob";
+        n = size(stoneValue);
+        dp.resize(n + 1, -1);
+        int ans = f(0, stoneValue);
+        if(ans > 0) return "Alice";
+        else if(ans < 0) return "Bob";
         else return "Tie";
     }
 };
