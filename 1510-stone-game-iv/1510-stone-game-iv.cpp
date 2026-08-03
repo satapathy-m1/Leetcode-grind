bcd03ddddb2aa879f0ac17612dff7a86786421a6
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-08-03 14:35:16 +0530

    Time: 43 ms (54.94%), Space: 16.8 MB (36.88%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    vector<int> dp;
+    bool solve(int n) {
+        if(n < 1) return false;
+        if(dp[n] != -1) return dp[n]; 
+
+        for(int i = 1; i * i <= n; i++) {
+            if(!solve(n - i*i)) return dp[n] = true;
+        }
+
+        return dp[n] = false;
+    }
+public:
+    bool winnerSquareGame(int n) {
+        dp.resize(n + 1, -1);
+        return solve(n);
+    }
+};
