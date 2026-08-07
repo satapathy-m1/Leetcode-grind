ff08983f7213b866cb0b861330b5b090a34c58e0
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-08-07 22:00:41 +0530

    Time: 96 ms (35.9%), Space: 182.1 MB (33.66%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,43 +1,41 @@
 class LRUCache {
 private:
-    list<int> dll;
-    unordered_map<int, pair<int, list<int> :: iterator>> mpp;
-    int cap;
+    int n;
     int currSize;
+    unordered_map<int, pair<list<int>::iterator, int>> mpp;
+    list<int> dll;
 public:
     LRUCache(int capacity) {
-        this -> cap = capacity;
+        n = capacity;
         currSize = 0;
     }
     
     int get(int key) {
-        if(mpp.find(key) == mpp.end()) return -1;
-        auto [val, add] = mpp[key];
+        if(!mpp.count(key)) return -1;
+        auto [add, val] = mpp[key];
         dll.erase(add);
         dll.push_front(key);
-        mpp[key] = {val, dll.begin()};
+        mpp[key] = {dll.begin(), val};
         return val;
     }
     
     void put(int key, int value) {
-        if(mpp.find(key) != mpp.end()) {
-            auto [val, add] = mpp[key];
+        if(mpp.count(key)) {
+            auto [add, val] = mpp[key];
             dll.erase(add);
             dll.push_front(key);
-            mpp[key] = {value, dll.begin()};
+            mpp[key] = {dll.begin(), value};
+            return;
         }
-        else if(currSize == cap) {
-            int lruKey = dll.back();
-            mpp.erase(lruKey);
+        if(currSize == n) {
+            int keyToDel = dll.back();
             dll.pop_back();
-            dll.push_front(key);
-            mpp[key] = {value, dll.begin()};
+            mpp.erase(keyToDel);
+            currSize--;
         }
-        else {
-            dll.push_front(key);
-            mpp[key] = {value, dll.begin()};
-            currSize++;
-        }
+        dll.push_front(key);
+        mpp[key] = {dll.begin(), value};
+        currSize++;
     }
 };
 
