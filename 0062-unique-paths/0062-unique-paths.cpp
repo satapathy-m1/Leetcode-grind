db3606775187aec8ad11ea2eed66dce48ee64cc7
Mrutyunjaya Satapathy <144725918+satapathy-m1@users.noreply.github.com>
2026-08-13 16:48:52 +0530

    Time: 0 ms (100%), Space: 9.4 MB (55.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,25 @@
 class Solution {
-public:
-    int f(int i, int j, int m, int n, vector<vector<int>>& dp) {
-        if(i >= m || j >= n) return 0;
+private:
+    int m, n;
+    vector<vector<int>> dp;
+    int f(int i, int j) {
         if(i == m - 1 && j == n - 1) return 1;
+        if(i >= m || j >= n) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int ans = f(i + 1, j, m, n, dp) + f(i, j + 1, m, n, dp);
-        return dp[i][j] = ans;
+        return dp[i][j] = f(i + 1, j) + f(i, j + 1);
     }
+public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        return f(0, 0, m, n, dp);
+        this -> m = m;
+        this -> n = n;
+        dp.assign(m + 1, vector<int>(n + 1, 0));
+
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if(i == m - 1 && j == n - 1) dp[i][j] = 1;
+                else dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+        return dp[0][0];
     }
 };
